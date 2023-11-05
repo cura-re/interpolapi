91615af2710f0b446c8f457afd70bc686433ffde
Blehjo <blehjooo@gmail.com>
2023-11-05 01:14:28 -0600

    added controllers based off models

---
+++ Controllers/CommunitiesController.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using interpolapi.Data;
+using interpolapi.Models;
+
+namespace interpolapi.Controllers
+{
+    public class CommunitiesController : Controller
+    {
+        private readonly InterpolContext _context;
+
+        public CommunitiesController(InterpolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Communities
+        public async Task<IActionResult> Index()
+        {
+            var interpolContext = _context.Communities.Include(c => c.Photo).Include(c => c.User);
+            return View(await interpolContext.ToListAsync());
+        }
+
+        // GET: Communities/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Communities == null)
+            {
+                return NotFound();
+            }
+
+            var community = await _context.Communities
+                .Include(c => c.Photo)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.CommunityId == id);
+            if (community == null)
+            {
+                return NotFound();
+            }
+
+            return View(community);
+        }
+
+        // GET: Communities/Create
+        public IActionResult Create()
+        {
+            ViewData["PhotoId"] = new SelectList(_context.Photos, "PhotoId", "PhotoId");
+            ViewData["UserId"] = new SelectList(_context.InterpolUsers, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Communities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CommunityId,CommunityName,CommunityDescription,DateCreated,UserId,PhotoId")] Community community)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(community);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PhotoId"] = new SelectList(_context.Photos, "PhotoId", "PhotoId", community.PhotoId);
+            ViewData["UserId"] = new SelectList(_context.InterpolUsers, "UserId", "UserId", community.UserId);
+            return View(community);
+        }
+
+        // GET: Communities/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Communities == null)
+            {
+                return NotFound();
+            }
+
+            var community = await _context.Communities.FindAsync(id);
+            if (community == null)
+            {
+                return NotFound();
+            }
+            ViewData["PhotoId"] = new SelectList(_context.Photos, "PhotoId", "PhotoId", community.PhotoId);
+            ViewData["UserId"] = new SelectList(_context.InterpolUsers, "UserId", "UserId", community.UserId);
+            return View(community);
+        }
+
+        // POST: Communities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CommunityId,CommunityName,CommunityDescription,DateCreated,UserId,PhotoId")] Community community)
+        {
+            if (id != community.CommunityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(community);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommunityExists(community.CommunityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PhotoId"] = new SelectList(_context.Photos, "PhotoId", "PhotoId", community.PhotoId);
+            ViewData["UserId"] = new SelectList(_context.InterpolUsers, "UserId", "UserId", community.UserId);
+            return View(community);
+        }
+
+        // GET: Communities/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Communities == null)
+            {
+                return NotFound();
+            }
+
+            var community = await _context.Communities
+                .Include(c => c.Photo)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.CommunityId == id);
+            if (community == null)
+            {
+                return NotFound();
+            }
+
+            return View(community);
+        }
+
+        // POST: Communities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Communities == null)
+            {
+                return Problem("Entity set 'InterpolContext.Communities'  is null.");
+            }
+            var community = await _context.Communities.FindAsync(id);
+            if (community != null)
+            {
+                _context.Communities.Remove(community);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CommunityExists(string id)
+        {
+          return (_context.Communities?.Any(e => e.CommunityId == id)).GetValueOrDefault();
+        }
+    }
+}
